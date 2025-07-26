dceac0d895b367e53d17ee562e687299afe753f4
bugCreator <anh.dt1510@gmail.com>
2025-07-26 18:19:40 +0700

    add h2 database

---
+++ src/main/java/bug/creator/studentmanagement/entity/Student.java
@@ -0,0 +1,61 @@
+package bug.creator.studentmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @Column(name = "id_sv")
+    private Integer idSv;
+
+    @Column(name = "university")
+    private String university;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date_of_birth")
+    private LocalDateTime dateOfBirth;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "major")
+    private String major;
+
+    @Column(name = "major_code")
+    private String majorCode;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "course")
+    private Integer course;
+
+    @Column(name = "class")
+    private String className; // dùng tên khác để tránh từ khóa Java
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phonge_number")
+    private Long phoneNumber; // đổi từ int sang Long để chứa số dài
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "training_system")
+    private String trainingSystem;
+}
